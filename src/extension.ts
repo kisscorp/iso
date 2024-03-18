0f55b8d6aec157091a92610de96761abd8ca5ff9
druid <angular.druid@gmail.com>
2024-03-18 17:47:00 -0400

    Add file watcher to log file names that have changed within 10 seconds debounce.

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let timeout: NodeJS.Timeout | undefined = undefined;
+let changedFiles: Set<string> = new Set();
+
 export function activate(context: vscode.ExtensionContext) {
+		vscode.window.showInformationMessage('Congratulations, your extension "ch24-test-ext" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ch24-test-ext" is now active!');
+    const watcher = vscode.workspace.createFileSystemWatcher('**', false, false, false);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ch24-test-ext.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ch24-test-ext!');
-	});
+    watcher.onDidChange(e => {
+        changedFiles.add(path.basename(e.fsPath));
 
-	context.subscriptions.push(disposable);
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+
+        timeout = setTimeout(() => {
+						vscode.window.showInformationMessage(`Files changed in the last 10 seconds: ${Array.from(changedFiles).join(", ")}`);
+            changedFiles.clear();
+        }, 10000);
+    });
+
+    context.subscriptions.push(watcher);
+
+    let disposable = vscode.commands.registerCommand('ch24-test-ext.helloWorld', () => {
+        vscode.window.showInformationMessage('Hello World from ch24-test-ext!');
+    });
+
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
