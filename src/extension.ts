eda1857e9fca4029dfd7c0c6cbecd53f40f7b9b1
druid <angular.druid@gmail.com>
2024-04-08 14:50:52 -0400

    Stable execution of radon, cpd, and bandit.

--- src/extension.ts
+++ src/extension.ts
@@ -6,19 +6,43 @@ const outputChannel = vscode.window.createOutputChannel('Docker Logs');
 
 const runDockerComposeTask = () => {
 	const type: string = 'shell';
-	const command: string = 'docker-compose up my-python-service my-python-service2 my-iso-radon-service';
+	const command: string = 'docker-compose up my-iso-radon-service my-iso-cpd-service my-iso-bandit-service';
 	const problemMatcher: string[] = [];
-	const executionOptions: vscode.ShellExecutionOptions = { cwd: __dirname };
+
+  if (vscode.workspace.workspaceFolders !== undefined) {
+		process.env['WORKSPACE_PATH'] = vscode.workspace.workspaceFolders[0].uri.fsPath;
+	}
+
+	const env: any = {};
+	for (let key in process.env) {
+			if (process.env[key] !== undefined) {
+					env[key] = process.env[key];
+			}
+	}
+
+	const executionOptions: vscode.ShellExecutionOptions = { 
+			cwd: __dirname,
+			env: process.env as any
+	};
+
 	const dockerTask: vscode.Task = new vscode.Task({ type: type }, vscode.TaskScope.Workspace, 'docker-up', 'extension-source', new vscode.ShellExecution(command, executionOptions), problemMatcher);
+
+	if (vscode.workspace.workspaceFolders !== undefined) {
+      process.env['WORKSPACE_PATH'] = vscode.workspace.workspaceFolders[0].uri.fsPath;
+  }
 
 	vscode.tasks.executeTask(dockerTask).then(() => {
 			vscode.window.showInformationMessage('Docker container finished executing!');
 			printDockerLogs('my-python-service');
 			printDockerLogs('my-python-service2');
 			printDockerLogs('my-iso-radon-service');
+			printDockerLogs('my-iso-cpd-service');
+			printDockerLogs('my-iso-bandit-service');
 			printContainerStatus('my-python-service');
 			printContainerStatus('my-python-service2');
 			printContainerStatus('my-iso-radon-service');
+			printContainerStatus('my-iso-cpd-service');
+			printContainerStatus('my-iso-bandit-service');
 	});
 };
 
@@ -59,6 +83,7 @@ const debounce = (func: () => void, delay: number) => {
 };
 
 const debouncedRunDockerComposeTask = debounce(runDockerComposeTask, 10000);
+
 
 export function activate(context: vscode.ExtensionContext) {
     vscode.window.showInformationMessage('Congratulations, your extension "ch24-test-ext" is now active!');
